f10082b0143917dacf4e623cfe26dd58ba72eb36
Lorenzo Giuliani <lorenzo@frenzart.com>
2019-03-04 18:41:59 +0100

    seed using a transaction

--- src/marrano_bot/marrano.clj
+++ src/marrano_bot/marrano.clj
@@ -25,20 +25,24 @@
   (do (println ">> registered webhook URL: " webhook-url)
       (t/set-webhook token webhook-url)
       ;; seed database
-      (c/assoc-at! db [:commands]
-                   {"marrano"   "%, sei un marrano!",
-                    "schif"     "%, io ti schifo!",
-                    "betaschif" "%, io ti betaschifo!",
-                    "strunz"    "%, sei strunz!",
-                    "paris"     "%, sei più helpy di paris hilton!",
-                    "chain"     "%, sei più lento di una blockchain!",
-                    "cripto"    "%, ti criptobottokremlino!",
-                    "soviet"    "%, ti mando a Stalingrado!",
-                    "russa"     "%, deh or dico a Putin di tolgliert le russacchiotte di man!",
-                    "spec"      "%, ti fo crashare pur di non cambiare la mia spec.",
-                    "bot"       "mannò, massù, sù!"})
-      (c/assoc-at! db [:slap] ["una grande trota!"
-                               "le diciotto bobine edizione limitata de La Corazzata Potemkin durante Italia Inghilterra"])))
+      (c/with-write-transaction [db tx]
+        (-> tx
+            (c/assoc-at [:commands]
+                        {"marrano"   "%, sei un marrano!",
+                         "schif"     "%, io ti schifo!" ,
+                         "betaschif" "%, io ti betaschifo!" ,
+                         "strunz"    "%, sei strunz!" ,
+                         "paris"     "%, sei più helpy di paris hilton!" ,
+                         "chain"     "%, sei più lento di una blockchain!" ,
+                         "cripto"    "%, ti criptobottokremlino!" ,
+                         "soviet"    "%, ti mando a Stalingrado!" ,
+                         "russa"     "%, deh or dico a Putin di tolgliert le russacchiotte di man!" ,
+                         "spec"      "%, ti fo crashare pur di non cambiare la mia spec." ,
+                         "bot"       "mannò, massù, sù!"})
+            (c/update-at [:slap]
+                         ["una grande trota!"
+                          "le diciotto bobine edizione limitata de La Corazzata Potemkin durante Italia Inghilterra"
+                          "con una BlockChain AI WebScale BigData!!!111"])))))
 
 (defn- template
   [tpl text]
