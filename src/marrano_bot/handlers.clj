bd55718ea3605ab4ca27188f393052ec24cebbc6
Lorenzo Giuliani <lorenzo@frenzart.com>
2020-07-23 10:27:49 +0000

    text can be nil

--- src/marrano_bot/handlers.clj
+++ src/marrano_bot/handlers.clj
@@ -15,10 +15,11 @@
 
 (defn telegram-handler [r]
   (let [body (:body-params r)
-        message (:messge body)]
+        message (merge {:text ""} ; text can be nil!!!
+                       (:messge body))]
     (debug "body" body)
     {:status 200
-     :body (bot-api (get-in r [:body-params :message]))}))
+     :body (bot-api message)}))
 
 (def stack
   (ring/ring-handler
@@ -47,4 +48,13 @@
                 :body {:message {:chat {:id 123}}
                                 :text "/paris"}})
         (:body)
-        (slurp))))
+        (slurp))
+
+    {:update_id 82255996,
+     :message {:date 1595448322,
+               :animation {:file_name "mp4.mp4", :mime_type "video/mp4",
+                           :width 258, :duration 3, :file_size 93745,
+                           :file_unique_id "AgADGwIAAlt-xFI",
+                           :file_id "CgACAgQAAxkBAAEDPYNfGJwCnelTctoBQPONU6bO9UynswACGwIAAlt-xFIsrwcfAAEn19oaBA",
+                           :height 148},
+               :chat {:type "group", :title "Marrani Unlimited Ltd, l'angolo delle russacchiotte, polacchine, ucrainine, estonine e ungheresine", :id -284819895, :all_members_are_administrators true}, :document {:file_name "mp4.mp4", :mime_type "video/mp4", :file_size 93745, :file_unique_id "AgADGwIAAlt-xFI", :file_id "CgACAgQAAxkBAAEDPYNfGJwCnelTctoBQPONU6bO9UynswACGwIAAlt-xFIsrwcfAAEn19oaBA"}, :message_id 212355, :from {:first_name "Luca", :is_bot false, :username "LuKeLuky", :id 467996968, :last_name "Bertolani"}, :via_bot {:first_name "Tenor GIF Search", :is_bot true, :username "gif", :id 140267078}}}))
