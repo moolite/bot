3c5856ef243a03470f33be73c175a346719922d5
Lorenzo Giuliani <lorenzo@frenzart.com>
2023-03-20 18:35:25 +0100

    remember to escape things

--- src/moolite/bot/actions.clj
+++ src/moolite/bot/actions.clj
@@ -150,8 +150,8 @@
                         (db/execute-one!))]
       (debug "item" item)
       (condp = (:kind item)
-        "photo" (send/photo gid (:data item) (:description item))
-        "video" (send/video gid (:data item) (:description item))))))
+        "photo" (send/photo gid (:data item) (message/escape (:description item)))
+        "video" (send/video gid (:data item) (message/escape (:description item)))))))
 
 (defn list-abraxas
   [gid]
