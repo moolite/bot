131407adc6cdb9dd0252371f829b81f3f1ecb154
Lorenzo Giuliani <lorenzo@frenzart.com>
2025-06-18 22:29:29 +0200

    missing commas

--- internal/db/channel_stats.go
+++ internal/db/channel_stats.go
@@ -42,7 +42,7 @@ func SelectChannelStatsStats(ctx context.Context, gid int64) (*ChannelStatsStats
 			MIN(points) AS min,
 			MAX(points) AS max,
 			SUM(points) AS sum,
-			CAST(ROUND(AVG(points)) AS INTEGER) AS avg
+			CAST(ROUND(AVG(points)) AS INTEGER) AS avg,
 			COUNT(points) AS count,
 		FROM channel_stats
 		WHERE gid = ?
